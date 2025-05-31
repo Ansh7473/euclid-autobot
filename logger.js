d1f911897615b2d4f7b28b14009216fd8fe40090
YourAnsh <88235820+Ansh7473@users.noreply.github.com>
2025-05-31 13:34:38 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -20,7 +20,7 @@ const logger = {
   banner: () => {
     console.log(`${colors.cyan}${colors.bold}`);
     console.log(`-------------------------------------------------`);
-    console.log(`   Euclid Testnet Auto Bot - Airdrop Insiders`);
+    console.log(`   Euclid Testnet Auto Bot - FOR Airdrop Insiders`);
     console.log(`-------------------------------------------------${colors.reset}`);
     console.log();
   }
